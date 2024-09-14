97f51602b76c6b25beab2b0630402f697b4a4ae6
Diego Hernández <diego.hernandez@encora.com>
2024-09-14 01:15:30 -0600

    build:Adds MongoDb user initialization to init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+echo "Builds whole project"
 docker-compose up --build --detach
 
-echo "Project initialized. Backend built and Docker containers started."
+echo "Waits for MongoDB to start"
+sleep 10
+
+echo "Connects to MongoDB and creates new user"
+docker-compose exec mongodb mongosh --username root --password example \
+  --eval 'use admin' \
+  --eval 'const username = "taskManagerUser"' \
+  --eval 'const existingUser = db.system.users.findOne({ user: username })' \
+  --eval 'if (existingUser) db.dropUser(username)' \
+  --eval 'db.createUser({
+            user: username,
+            pwd: "TaskUser847584",
+            roles: [
+              { role: "readWrite", db: "admin" },
+              { role: "readWrite", db: "task-manager-db" },
+            ],
+          })'
+
+echo "Project initialized."
