e88e2c95fd3bdb234cba03f2d0aba0302da66a08
Diego Hernández <diego.hernandez@encora.com>
2024-09-16 22:28:14 -0600

    feat:Implements UserService with just a getUserById method

---
+++ backend/src/main/java/com/encora/taskmanager/service/UserService.java
@@ -0,0 +1,9 @@
+package com.encora.taskmanager.service;
+
+import com.encora.taskmanager.model.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    Optional<User> getUserById(Long id);
+}
---
+++ backend/src/main/java/com/encora/taskmanager/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.encora.taskmanager.service;
+
+import com.encora.taskmanager.exception.TaskManagerException;
+import com.encora.taskmanager.model.User;
+import com.encora.taskmanager.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        try {
+            return userRepository.findById(id);
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving user with ID: {}", id, e);
+            throw new TaskManagerException("Error retrieving user", e);
+        }
+    }
+}
